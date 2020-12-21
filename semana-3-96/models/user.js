df23d79b44f73a9797ce67a8be17c2db9166d3dd
carlospava <75138486+carlospava@users.noreply.github.com>
2020-12-20 19:03:56 -0500

    Add files via upload

---
+++ semana-3-96/models/user.js
@@ -0,0 +1,21 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    
+    static associate(models) {
+      
+    }
+  };
+  user.init({
+    name: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+  return user;
+};
